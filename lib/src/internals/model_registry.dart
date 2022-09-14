c7772df2ff2c84b28b77a17ed2df5428882c2092
Tim Whiting <tim@whitings.org>
2022-09-14 14:46:19 -0600

    fix date time issue

--- lib/src/internals/model_registry.dart
+++ lib/src/internals/model_registry.dart
@@ -31,7 +31,9 @@ class _PrimitiveTypeConverter<T> implements TypeConverter<T> {
 class _DateTimeConverter implements TypeConverter<DateTime> {
   @override
   DateTime decode(dynamic d) {
-    if (d is String) {
+    if (d is DateTime) {
+      return d;
+    } else if (d is String) {
       return DateTime.parse(d);
     } else if (d is num) {
       return DateTime.fromMillisecondsSinceEpoch(d.round());
